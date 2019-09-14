111f792cf1e88bf7762c5a9b6d95545c66027bb4
Mickaël Bergem <mickael@securem.eu>
2019-09-13 18:41:01 -0700

    Use doctests in Python module

--- howfast_apm/utils.py
+++ howfast_apm/utils.py
@@ -4,8 +4,23 @@ from typing import List
 
 
 def is_in_blacklist(uri: str, blacklist: List[str]) -> bool:
-    """ Return True if the URI is blacklisted """
+    """
+    Return True if the URI is blacklisted.
+
+      >>> is_in_blacklist('/some-real-uri/', ['/exact-uri/'])
+      False
+      >>> is_in_blacklist('/exact-uri/', ['/exact-uri/'])
+      True
+      >>> is_in_blacklist('/job/42/retry', ['/job/*'])
+      True
+
+    """
     for pattern in blacklist:
         if fnmatch.fnmatch(uri, pattern):
             return True
     return False
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
